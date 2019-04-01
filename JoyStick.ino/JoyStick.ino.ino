ebe8f97bdc355e779dad92af83337204bbb9e551
sarahc123 <g00330821@gmit.ie>
2019-04-01 18:07:26 +0100

    steering now recenters itself

--- JoyStick.ino/JoyStick.ino.ino
+++ JoyStick.ino/JoyStick.ino.ino
@@ -10,19 +10,40 @@ int joyVal;
  
 void setup() 
 { 
- servo1.attach (9);    // Steering Servo
- servo2.attach (10);   // Speed Servo
+ servo1.attach (9);    // Steering Servo didgital pin
+ servo2.attach (11);   // Speed Servo didgital pin
 } 
 
 void loop()
 
 {
- joyVal = analogRead (joyX);
- joyVal = map (joyVal, 0, 1023, 0, 180); 
- servo1.write (joyVal);
  
- joyVal = analogRead (joyY);
- servo1.write (joyVal);
+  // Steering Servo
+  // mapped from pot 0-1023 to 1-180 so its a value in degrees but split in the middle to get left and right, re-centre
+ {
+ joyVal = analogRead (joyX);              
  
- delay (15);
+ if (joyX < 480)
+ {
+ joyVal = map (joyVal, 0, 511, 50, 90);    // right caibrated to not over reach to the right
+ servo1.write (joyVal);
+ delay (5);
+ }
+ else if (joyX > 530)
+ {
+ joyVal = map (joyVal, 513, 1023, 90, 180); //left caibrated to not over reach to the left
+ servo1.write (joyVal);
+ delay (5);
+ }
+}
+ 
+ 
+ // Speed servo 
+ 
+ 
+ //joyVal = analogRead (joyY);               
+ //joyVal = map (joyVal, 530, 1023, 0, 255);   // once the joystick is moved forward past 530 its maps the motor speed form 0 to 255
+ //servo1.write (joyVal);
+ 
+ //delay (5);
 }
